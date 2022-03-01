162af7fbfe08f3896fb1a4e1db0925836e674f9b
DreSean-Sutton <dreseansutton@gmail.com>
2022-03-01 12:01:01 -0800

    Work in progress

--- js/data.js
+++ js/data.js
@@ -1,1 +1,310 @@
 /* exported data */
+const data = {
+  view: 'characterList',
+  characters: [
+    mario = {
+      name: 'mario',
+      imgURL: ''
+    },
+    donkeyKong = {
+      name: 'donkeyKong',
+      imgURL:
+    },
+    link = {
+      name: 'link',
+      imgURL:
+    },
+    samus = {
+      name: 'samus',
+      imgURL:
+    },
+    darkSamus = {
+      name: 'dark samus',
+      imgURL:
+    },
+    yoshi = {
+      name: 'yoshi',
+      imgURL:
+    },
+    kirby = {
+      name: 'kirby',
+      imgURL:
+    },
+    fox = {
+      name: 'fox',
+      imgURL:
+    },
+    pikachu = {
+      name: 'pikachu',
+      imgURL:
+    },
+    luigi = {
+      name: 'luigi',
+      imgURL:
+    },
+    ness = {
+      name: 'ness',
+      imgURL:
+    },
+    captainFalcon = {
+      name: 'captainFalcon',
+      imgURL:
+    },
+    jigglypuff = {
+      name: 'jigglypuff',
+      imgURL:
+    },
+    peach = {
+      name: 'peach',
+      imgURL:
+    },
+    daisy = {
+      name: 'daisy',
+      imgURL:
+    },
+    bowser = {
+      name: 'bowser',
+      imgURL:
+    },
+    iceClimbers = {
+      name: 'iceClimbers',
+      imgURL:
+    },
+    sheik = {
+      name: 'sheik',
+      imgURL:
+    },
+    zelda = {
+      name: 'zelda',
+      imgURL:
+    },
+    drMario = {
+      name: 'drMario',
+      imgURL:
+    },
+    pichu = {
+      name: 'pichu',
+      imgURL:
+    },
+    falco = {
+      name: 'falco',
+      imgURL:
+    },
+    marth = {
+      name: 'marth',
+      imgURL:
+    },
+    lucina = {
+      name: 'Lucina',
+      imgURL:
+    },
+    youngLink = {
+      name: 'youngLink',
+      imgURL:
+    },
+    ganondorf = {
+      name: 'ganondorf',
+      imgURL:
+    },
+    mewtwo = {
+      name: 'mewtwo',
+      imgURL:
+    },
+    roy = {
+      name: 'roy',
+      imgURL:
+    },
+    chrom = {
+      name: 'chrom',
+      imgURL:
+    },
+    mrGameWatch = {
+      name: 'mrGameWatch',
+      imgURL:
+    },
+    metaKnight = {
+      name: 'metaKnight',
+      imgURL:
+    },
+    pit = {
+      name: 'pit',
+      imgURL:
+    },
+    darkPit = {
+      name: 'darkPit',
+      imgURL:
+    },
+    zeroSuitSamus = {
+      name: 'zeroSuitSamus',
+      imgURL:
+    },
+    wario = {
+      name: 'wario',
+      imgURL:
+    },
+    snake = {
+      name: 'snake',
+      imgURL:
+    },
+    ike = {
+      name: 'ike',
+      imgURL:
+    },
+    squirtle = {
+      name: 'squirtle',
+      imgURL:
+    },
+    ivysaur = {
+      name: 'ivysaur',
+      imgURL:
+    },
+    charizard = {
+      name: 'charizard',
+      imgURL:
+    },
+    diddyKong = {
+      name: 'diddyKong',
+      imgURL:
+    },
+    lucas = {
+      name: 'lucas',
+      imgURL:
+    },
+    sonic = {
+      name: 'sonic',
+      imgURL:
+    },
+    king Dedede = {
+      name: 'kingDedede',
+      imgURL:
+    },
+    olimar = {
+      name: 'olimar',
+      imgURL:
+    },
+    lucario = {
+      name: 'lucario',
+      imgURL:
+    },
+    rob = {
+      name: 'rob',
+      imgURL:
+    },
+    toonLink = {
+      name: 'toonLink',
+      imgURL:
+    },
+    wolf = {
+      name: 'wolf',
+      imgURL:
+    },
+    villager = {
+      name: 'villager',
+      imgURL:
+    },
+    megaMan = {
+      name: 'megaMan',
+      imgURL:
+    },
+    wiiFitTrainer = {
+      name: 'wiiFitTrainer',
+      imgURL:
+    },
+    rosalinaLuma = {
+      name: 'rosalinaLuma',
+      imgURL:
+    },
+    little Mac = {
+      name: 'littleMac',
+      imgURL:
+    },
+    greninja = {
+      name: 'greninja',
+      imgURL:
+    },
+    miiBrawler = {
+      name: '/ Mii Fighter(Brawler',
+      imgURL:
+    },
+    miiSwordfighter = {
+      name: '/ Mii Fighter(Swordfighter',
+      imgURL:
+    },
+    miiGunner = {
+      name: '/ Mii Fighter(Gunner',
+      imgURL:
+    },
+    palutena = {
+      name: 'palutena',
+      imgURL:
+    },
+    pacMan = {
+      name: 'pacMan',
+      imgURL:
+    },
+    robin = {
+      name: 'robin',
+      imgURL:
+    },
+    shulk = {
+      name: 'shulk',
+      imgURL:
+    },
+    bowerJr = {
+      name: 'BowserJr',
+      imgURL:
+    },
+    duckHunt = {
+      name: 'duckHunt',
+      imgURL:
+    },
+    ryu = {
+      name: 'ryu',
+      imgURL:
+    },
+    ken = {
+      name: 'ken',
+      imgURL:
+    },
+    cloud = {
+      name: 'cloud',
+      imgURL:
+    },
+    corrin = {
+      name: 'corrin',
+      imgURL:
+    },
+    bayonetta = {
+      name: 'bayonetta',
+      imgURL:
+    },
+    inkling = {
+      name: 'inkling',
+      imgURL:
+    },
+    ridley = {
+      name: 'ridley',
+      imgURL:
+    },
+    simon = {
+      name: 'simon',
+      imgURL:
+    },
+    richter = {
+      name: 'richter',
+      imgURL:
+    },
+    kingKRool = {
+      name: 'KingKRool',
+      imgURL:
+    },
+    isabelle = {
+      name: 'isabelle',
+      imgURL:
+    },
+    incineroa = {
+      name: 'incineroar',
+      imgURL:
+    }
+  ]
+};
