e065c4da950d1bbba615f6ce19d9dbfed6b94f1c
DreSean-Sutton <dreseansutton@gmail.com>
2022-03-01 16:51:18 -0800

    Characters are correctly being rendered to the page when renderCharacterCard function is called

--- js/data.js
+++ js/data.js
@@ -1,543 +1,541 @@
 /* exported data */
 const data = {
   view: 'characterList',
-  characters: {
-    mario: {
+  characters: [
+    {
       name: 'mario',
       imgURL: 'chara_5_mario_00.png',
       id: 1,
       number: 1
     },
-    donkeyKong: {
+    {
       name: 'donkeyKong',
       imgURL: 'chara_5_donkey_kong_00.png',
       id: 2,
       number: 2
 
     },
-    link: {
+    {
       name: 'link',
       imgURL: 'chara_5_link_00.png',
       id: 3,
       number: 3
     },
-    samus: {
+    {
       name: 'samus',
       imgURL: 'chara_5_samus_00.png',
       id: 4,
       number: 4
     },
-    darkSamus: {
+    {
       name: 'darkSamus',
       imgURL: 'chara_5_dark_samus_00.png',
       id: 5,
       number: 4
     },
-    yoshi: {
+    {
       name: 'yoshi',
       imgURL: 'chara_5_yoshi_07.png',
       id: 6,
       number: 5
     },
-    kirby: {
+    {
       name: 'kirby',
       imgURL: 'chara_5_kirby_00.png',
       id: 7,
       number: 6
     },
-    fox: {
+    {
       name: 'fox',
       imgURL: 'chara_5_fox_00.png',
       id: 8,
       number: 7
     },
-    pikachu: {
+    {
       name: 'pikachu',
       imgURL: 'chara_5_pikachu_00.png',
       id: 9,
       number: 8
     },
-    luigi: {
+    {
       name: 'luigi',
       imgURL: 'chara_5_luigi_00.png',
       id: 10,
       number: 9
     },
-    ness: {
+    {
       name: 'ness',
-      imgURL: 'chara_5_ness_00.png',
+      imgURL: 'chara_5_ness_satan_00.png',
       id: 11,
       number: 10
     },
-    captainFalcon: {
+    {
       name: 'captainFalcon',
       imgURL: 'chara_5_captain_falcon_00.png',
       id: 12,
       number: 11
     },
-    jigglypuff: {
+    {
       name: 'jigglypuff',
       imgURL: 'chara_5_jigglypuff_00.png',
       id: 13,
       number: 12
     },
-    peach: {
+    {
       name: 'peach',
       imgURL: 'chara_5_peach_00.png',
       id: 14,
       number: 13
     },
-    daisy: {
+    {
       name: 'daisy',
       imgURL: 'chara_5_daisy_00.png',
       id: 15,
       number: 13
     },
-    bowser: {
+    {
       name: 'bowser',
       imgURL: 'chara_5_bowser_00.png',
       id: 16,
       number: 14
     },
-    iceClimbers: {
+    {
       name: 'iceClimbers',
       imgURL: 'chara_5_ice_climbers_00.png',
       id: 17,
       number: 15
     },
-    sheik: {
+    {
       name: 'sheik',
       imgURL: 'chara_5_sheik_00.png',
       id: 18,
       number: 16
     },
-    zelda: {
+    {
       name: 'zelda',
       imgURL: 'chara_5_zelda_00.png',
       id: 19,
       number: 17
     },
-    drMario: {
+    {
       name: 'drMario',
       imgURL: 'chara_5_dr_mario_00.png',
       id: 20,
       number: 18
     },
-    pichu: {
+    {
       name: 'pichu',
       imgURL: 'chara_5_pichu_00.png',
       id: 21,
       number: 19
     },
-    falco: {
+    {
       name: 'falco',
       imgURL: 'chara_5_falco_00.png',
       id: 22,
       number: 20
     },
-    marth: {
+    {
       name: 'marth',
       imgURL: 'chara_5_marth_00.png',
       id: 23,
       number: 21
     },
-    lucina: {
+    {
       name: 'Lucina',
-      imgURL: 'chara_5_lucina_00.png',
+      imgURL: 'chara_5_lucina_better_marth_00.png',
       id: 24,
       number: 21
     },
-    youngLink: {
+    {
       name: 'youngLink',
       imgURL: 'chara_5_young_link_00.png',
       id: 25,
       number: 22
     },
-    ganondorf: {
+    {
       name: 'ganondorf',
       imgURL: 'chara_5_ganondorf_00.png',
       id: 26,
       number: 23
     },
-    mewtwo: {
+    {
       name: 'mewtwo',
       imgURL: 'chara_5_mewtwo_00.png',
       id: 27,
       number: 24
     },
-    roy: {
+    {
       name: 'roy',
       imgURL: 'chara_5_roy_00.png',
       id: 28,
       number: 25
     },
-    chrom: {
+    {
       name: 'chrom',
       imgURL: 'chara_5_chrom_00.png',
       id: 29,
       number: 25
     },
-    mrGameWatch: {
+    {
       name: 'mrGameWatch',
-      imgURL: 'chara_5_mrGameWatch_00.png',
+      imgURL: 'chara_5_mr_game_watch_00.png',
       id: 30,
       number: 26
     },
-    metaKnight: {
+    {
       name: 'metaKnight',
       imgURL: 'chara_5_metaKnight_00.png',
       id: 31,
       number: 27
     },
-    pit: {
+    {
       name: 'pit',
       imgURL: 'chara_5_pit_00.png',
       id: 32,
       number: 28
     },
-    darkPit: {
+    {
       name: 'darkPit',
       imgURL: 'chara_5_darkpit_00.png',
       id: 33,
       number: 28
     },
-    zeroSuitSamus: {
+    {
       name: 'zeroSuitSamus',
       imgURL: 'chara_5_zss_00.png',
       id: 34,
       number: 29
     },
-    wario: {
+    {
       name: 'wario',
       imgURL: 'chara_5_wario_00.png',
       id: 35,
       number: 30
     },
-    snake: {
+    {
       name: 'snake',
       imgURL: 'chara_5_snake_00.png',
       id: 36,
       number: 31
     },
-    ike: {
+    {
       name: 'ike',
       imgURL: 'chara_5_ike_01.png',
       id: 37,
       number: 32
     },
-    squirtle: {
+    {
       name: 'squirtle',
       imgURL: 'chara_5_pokemon_squirtle_00.png',
       id: 38,
       number: 33
     },
-    ivysaur: {
+    {
       name: 'ivysaur',
       imgURL: 'chara_5_pokemon_ivysaur_00.png',
       id: 39,
       number: 34
     },
-    charizard: {
+    {
       name: 'charizard',
       imgURL: 'chara_5_pokemon_charizard_00.png',
       id: 40,
       number: 35
     },
-    diddyKong: {
+    {
       name: 'diddyKong',
-      imgURL: 'chara_5_didd y_kong_00.png',
+      imgURL: 'chara_5_diddy_kong_00.png',
       id: 41,
       number: 36
     },
-    lucas: {
+    {
       name: 'lucas',
       imgURL: 'chara_5_lucas_00.png',
       id: 42,
       number: 37
     },
-    sonic: {
+    {
       name: 'sonic',
       imgURL: 'chara_5_sonic_00.png',
       id: 43,
       number: 38
     },
-    kingDedede: {
+    {
       name: 'kingDedede',
       imgURL: 'chara_5_dedede_00.png',
       id: 44,
       number: 39
     },
-    olimar: {
+    {
       name: 'olimar',
       imgURL: 'chara_5_olimar_00.png',
       id: 45,
       number: 40
     },
-    lucario: {
+    {
       name: 'lucario',
       imgURL: 'chara_5_lucario_00.png',
       id: 46,
       number: 41
     },
-    rob: {
+    {
       name: 'rob',
       imgURL: 'chara_5_rob_01.png',
       id: 47,
       number: 42
     },
-    toonLink: {
+    {
       name: 'toonLink',
       imgURL: 'chara_5_toon_link_00.png',
       id: 48,
       number: 43
     },
-    wolf: {
+    {
       name: 'wolf',
       imgURL: 'chara_5_wolf_00.png',
       id: 49,
       number: 44
     },
-    villager: {
+    {
       name: 'villager',
       imgURL: 'chara_5_villager_00.png',
       id: 50,
       number: 45
     },
-    megaMan: {
+    {
       name: 'megaMan',
       imgURL: 'chara_5_megaman_00.png',
       id: 51,
       number: 46
     },
-    wiiFitTrainer: {
+    {
       name: 'wiiFitTrainer',
       imgURL: 'chara_5_wiifit_00.png',
       id: 52,
       number: 47
     },
-    rosalinaLuma: {
+    {
       name: 'rosalinaLuma',
       imgURL: 'chara_5_rosalina_luma_00.png',
       id: 53,
       number: 48
     },
-    littleMac: {
+    {
       name: 'littleMac',
       imgURL: 'chara_5_littlemac_00.png',
       id: 54,
       number: 49
     },
-    greninja: {
+    {
       name: 'greninja',
       imgURL: 'chara_5_greninja_00.png',
       id: 55,
       number: 50
     },
-    miiBrawler: {
+    {
       name: 'miiBrawler',
       imgURL: 'chara_5_miibrawler_00.png',
       id: 56,
       number: 51
     },
-    miiSwordfighter: {
+    {
       name: 'miiSwordfighter',
       imgURL: 'chara_5_miiswordman_00.png',
       id: 57,
       number: 52
     },
-    miiGunner: {
+    {
       name: 'miiGunner',
       imgURL: 'chara_5_miigunner_00.png',
       id: 58,
       number: 53
     },
-    palutena: {
+    {
       name: 'palutena',
       imgURL: 'chara_5_palutena_00.png',
       id: 59,
       number: 54
     },
-    pacMan: {
+    {
       name: 'pacMan',
       imgURL: 'chara_5_pacman_00.png',
       id: 60,
       number: 55
     },
-    robin: {
+    {
       name: 'robin',
       imgURL: 'chara_5_robin_00.png',
       id: 61,
       number: 56
     },
-    shulk: {
+    {
       name: 'shulk',
       imgURL: 'chara_5_shulk_00.png',
       id: 62,
       number: 57
     },
-    bowerJr: {
+    {
       name: 'BowserJr',
-      imgURL: 'chara_5_bower_jr_00.png',
+      imgURL: 'chara_5_bowser_jr_00.png',
       id: 63,
       number: 58
     },
-    duckHunt: {
+    {
       name: 'duckHunt',
       imgURL: 'chara_5_duckhunt_00.png',
       id: 64,
       number: 59
     },
-    ryu: {
+    {
       name: 'ryu',
       imgURL: 'chara_5_ryu_00.png',
       id: 65,
       number: 60
     },
-    ken: {
+    {
       name: 'ken',
       imgURL: 'chara_5_ken_00.png',
       id: 66,
       number: 60
     },
-    cloud: {
+    {
       name: 'cloud',
       imgURL: 'chara_5_cloud_01.png',
       id: 67,
       number: 61
     },
-    corrin: {
+    {
       name: 'corrin',
       imgURL: 'chara_5_corrin_00.png',
       id: 68,
       number: 62
     },
-    bayonetta: {
+    {
       name: 'bayonetta',
       imgURL: 'chara_5_bayonetta_00.png',
       id: 69,
       number: 63
     },
-    inkling: {
+    {
       name: 'inkling',
       imgURL: 'chara_5_inkling_00.png',
       id: 70,
       number: 64
     },
-    ridley: {
-      number: {
-        name: 'ridley',
-        imgURL: 'chara_5_ridl ey_00.png',
-        id: 71,
-        number: 65
-      },
-      simon: {
-        name: 'simon',
-        imgURL: 'chara_5_simon_00.png',
-        id: 72,
-        number: 66
-      },
-      richter: {
-        name: 'richter',
-        imgURL: 'chara_5_richter_00.png',
-        id: 73,
-        number: 66
-      },
-      kingKRool: {
-        name: 'KingKRool',
-        imgURL: 'chara_5_krool_00.png',
-        id: 74,
-        number: 67
-      },
-      isabelle: {
-        name: 'isabelle',
-        imgURL: 'chara_5_isabelle_00.png',
-        id: 75,
-        number: 68
-      },
-      incineroar: {
-        name: 'incineroar',
-        imgURL: 'chara_5_incineroar_00.png',
-        id: 76,
-        number: 69
-      },
-      piranhaPlant: {
-        name: 'piranhaPlant',
-        imgURL: 'chara_5_piranha_plant_00.png',
-        id: 77,
-        number: 70
-      },
-      joker: {
-        name: 'joker',
-        imgURL: 'chara_5_joker_00.png',
-        id: 78,
-        number: 71
-      },
-      hero: {
-        name: 'hero',
-        imgURL: 'chara_5_hero_01.png',
-        id: 79,
-        number: 72
-      },
-      banjo: {
-        name: 'banjo',
-        imgURL: 'chara_5_banjo_00.png',
-        id: 80,
-        number: 73
-      },
-      terry: {
-        name: 'terry',
-        imgURL: 'chara_5_terry_00.png',
-        id: 81,
-        number: 74
-      },
-      byleth: {
-        name: 'byleth',
-        imgURL: 'chara_5_byleth_00.png',
-        id: 82,
-        number: 75
-      },
-      minmin: {
-        name: 'minmin',
-        imgURL: 'chara_5_minmin_00.png',
-        id: 83,
-        number: 76
-      },
-      steve: {
-        name: 'steve',
-        imgURL: 'chara_5_steve_00.png',
-        id: 84,
-        number: 77
-      },
-      sephiroth: {
-        name: 'sephiroth',
-        imgURL: 'chara_5_sephiroth_00.png',
-        id: 85,
-        number: 78
-      },
-      pyra: {
-        name: 'pyra',
-        imgURL: 'chara_5_pyra_00.png',
-        id: 86,
-        number: 79
-      },
-      mythra: {
-        name: 'mythra',
-        imgURL: 'chara_5_mythra_00.png',
-        id: 87,
-        number: 80
-      },
-      kazuya: {
-        name: 'kazuya',
-        imgURL: 'chara_5_kazuya_01.png',
-        id: 88,
-        number: 81
-      },
-      sora: {
-        name: 'sora',
-        imgURL: 'chara_5_sora_04.png',
-        id: 89,
-        number: 82
-      }
+    {
+      name: 'ridley',
+      imgURL: 'chara_5_ridley_00.png',
+      id: 71,
+      number: 65
+    },
+    {
+      name: 'simon',
+      imgURL: 'chara_5_simon_00.png',
+      id: 72,
+      number: 66
+    },
+    {
+      name: 'richter',
+      imgURL: 'chara_5_richter_00.png',
+      id: 73,
+      number: 66
+    },
+    {
+      name: 'KingKRool',
+      imgURL: 'chara_5_krool_00.png',
+      id: 74,
+      number: 67
+    },
+    {
+      name: 'isabelle',
+      imgURL: 'chara_5_isabelle_00.png',
+      id: 75,
+      number: 68
+    },
+    {
+      name: 'incineroar',
+      imgURL: 'chara_5_incineroar_00.png',
+      id: 76,
+      number: 69
+    },
+    {
+      name: 'piranhaPlant',
+      imgURL: 'chara_5_piranha_plant_00.png',
+      id: 77,
+      number: 70
+    },
+    {
+      name: 'joker',
+      imgURL: 'chara_5_joker_00.png',
+      id: 78,
+      number: 71
+    },
+    {
+      name: 'hero',
+      imgURL: 'chara_5_hero_01.png',
+      id: 79,
+      number: 72
+    },
+    {
+      name: 'banjo',
+      imgURL: 'chara_5_banjo_00.png',
+      id: 80,
+      number: 73
+    },
+    {
+      name: 'terry',
+      imgURL: 'chara_5_terry_00.png',
+      id: 81,
+      number: 74
+    },
+    {
+      name: 'byleth',
+      imgURL: 'chara_5_byleth_01.png',
+      id: 82,
+      number: 75
+    },
+    {
+      name: 'minmin',
+      imgURL: 'chara_5_min_min_00.png',
+      id: 83,
+      number: 76
+    },
+    {
+      name: 'steve',
+      imgURL: 'chara_5_steve_00.png',
+      id: 84,
+      number: 77
+    },
+    {
+      name: 'sephiroth',
+      imgURL: 'chara_5_sephiroth_00.png',
+      id: 85,
+      number: 78
+    },
+    {
+      name: 'pyra',
+      imgURL: 'chara_5_pyra_00.png',
+      id: 86,
+      number: 79
+    },
+    {
+      name: 'mythra',
+      imgURL: 'chara_5_mythra_00.png',
+      id: 87,
+      number: 80
+    },
+    {
+      name: 'kazuya',
+      imgURL: 'chara_5_kazuya_01.png',
+      id: 88,
+      number: 81
+    },
+    {
+      name: 'sora',
+      imgURL: 'chara_5_sora_04.png',
+      id: 89,
+      number: 82
     }
-  }
+  ]
 };
--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
+/* eslint-disable no-unused-vars */
 /* exported data */
-const $test = document.querySelector('#test');
+const $characterList = document.querySelector('#character-list');
+// const $test = document.querySelector('#test');
 
-$test.src = `/images/smash-ultimate-sprites/${data.characters.bayonetta.imgURL}`;
-$test.alt = data.characters.bayonetta.name;
+// $test.alt = data.characters.bayonetta.name;
+
+const renderCharacterList = entry => {
+  // debugger;
+  const $cardColumn = document.createElement('div');
+  const $characterCard = document.createElement('div');
+  const $columnFull = document.createElement('div');
+  const $characterCardImg = document.createElement('img');
+  const $characterCardNum = document.createElement('span');
+  const $characterCardName = document.createElement('h3');
+
+  $cardColumn.classList = 'card-column';
+  $characterCard.classList = 'row character-card';
+  $columnFull.classList = 'column-full';
+  $characterCardImg.classList = 'character-card__img';
+  $characterCardNum.classList = 'character-card__number';
+  $characterCardName.classList = 'character-card__name';
+
+  $characterCardImg.src = `../images/smash-ultimate-sprites/${entry.imgURL}`;
+  if (entry.number < 10) {
+    $characterCardNum.textContent = `0${entry.number}`;
+  } else {
+    $characterCardNum.textContent = entry.number;
+  }
+  $characterCardName.textContent = entry.name;
+
+  $cardColumn.appendChild($characterCard);
+  $characterCard.appendChild($columnFull);
+  $columnFull.appendChild($characterCardImg);
+  $columnFull.appendChild($characterCardNum);
+  $columnFull.appendChild($characterCardName);
+  return $cardColumn;
+};
