7b7a050e6862af0e2eed1e72c8fddce50bc0f98c
DreSean-Sutton <dreseansutton@gmail.com>
2022-05-11 00:34:54 -0700

    Moved a bunch of files and fixed errors created by adding react functionality

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              '@babel/plugin-transform-react-jsx'
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
