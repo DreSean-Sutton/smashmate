120adc9b3c9d495b53397b8ea34b655597e7e815
DreSean-Sutton <dreseansutton@gmail.com>
2022-07-06 05:13:30 -0700

    Abandoning this idea for now and going to try hashrouter

--- api/build/index.js
+++ api/build/index.js
@@ -6,12 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path = require('path');
 const app = (0, express_1.default)();
-const port = 3001;
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+const port = 3000;
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../my-app/build/index.html'));
 });
-console.log(__dirname);
-console.log(__dirname);
 console.log(__dirname);
 app.listen(port, () => {
     // eslint-disable-next-line no-console
--- api/server/index.ts
+++ api/server/index.ts
@@ -1,14 +1,11 @@
 import express from 'express';
 const path = require('path');
 const app = express();
-const port = 3001
+const port = 3000
 
-app.get('/*', function (req: any, res: any) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('*', function (req: any, res: any) {
+  res.sendFile(path.join(__dirname, '../../my-app/build/index.html'));
 });
-
-console.log(__dirname);
-console.log(__dirname);
 console.log(__dirname);
 
 app.listen(port, () => {
