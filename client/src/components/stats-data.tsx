58cee598d89be0985352b90c7ab81fca0a4c0451
DreSean-Sutton <dreseansutton@gmail.com>
2022-07-13 04:35:24 -0700

    Added a server directory and moved entire create-react-app into a client directory

--- client/src/components/stats-data.tsx
+++ client/src/components/stats-data.tsx
@@ -5,14 +5,14 @@ import Loading from './loading';
 import FetchDataFail from './fetch-data-fail';
 import axios from 'axios';
 
-interface MovementDataProps {
+interface StatsDataProps {
   focusedFighter: FocusedFighter
 }
 interface FocusedFighter {
   fighterId: number
 }
-export default function MovementData(props: MovementDataProps) {
-  const [movements, setMovements] = useState([]);
+export default function StatsData(props: StatsDataProps) {
+  const [stats, setStats] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [fetchFailed, setFetchFailed] = useState(false);
 
@@ -20,9 +20,9 @@ export default function MovementData(props: MovementDataProps) {
     setIsLoading(true);
     async function fetchData() {
       try {
-        const res = await axios.get(`https://the-ultimate-api.herokuapp.com/api/fighters/data/movements?fighterId=${props.focusedFighter.fighterId}`)
+        const res = await axios(`https://the-ultimate-api.herokuapp.com/api/fighters/data/stats?fighterId=${props.focusedFighter.fighterId}`)
         if (res.status === 200) {
-          setMovements(res.data);
+          setStats(res.data);
         } else {
           throw Error();
         }
@@ -43,27 +43,25 @@ export default function MovementData(props: MovementDataProps) {
   }
   if (fetchFailed) {
     return (
-      <FetchDataFail data={'Dodges/Rolls'} />
+      <FetchDataFail data={'Stats'} />
     );
   } else {
-
-    const renderMovements = (movement: any) => {
+    const renderStats = (stat: any) => {
       return (
-        <React.Fragment key={movement.movementId}>
-          <Col className='p-3'>
+        <React.Fragment key={stat.statId}>
+          <Col className='p-3 text-center'>
             <Card className='p-2 bg-light text-dark typical-box-shadow text-capitalize'>
-              <Card.Title className='text-center fw-bold'>{movement.name}</Card.Title>
-              <p className='mb-0 pt-1 border-top'>Active Frames: {movement.activeFrames}</p>
-              <p className='mb-0 pt-1 border-top'>Total Frames: {movement.totalFrames}</p>
+              <Card.Title className='fw-bold'>{stat.name}</Card.Title>
+              <p className='mb-0 pt-1 border-top'>{stat.statValue}</p>
             </Card>
           </Col>
         </React.Fragment>
       );
     }
-    const allMovements = movements.map(renderMovements);
+    const allStats = stats.map(renderStats);
     return (
       <>
-        { allMovements }
+        { allStats };
       </>
     )
   }
