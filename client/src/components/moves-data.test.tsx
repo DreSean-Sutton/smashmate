6d73dd46aebec4c0250009385638efed4ea80ab2
DreSean-Sutton <dreseansutton@gmail.com>
2022-08-30 16:53:36 -0700

    Slightly modified some test. Trying to lower 200 status test's latency

--- client/src/components/moves-data.test.tsx
+++ client/src/components/moves-data.test.tsx
@@ -9,19 +9,26 @@ describe('Testing moves data fetch', () => {
 
   it('Correctly sends data on 200 status', async () => {
     const fetchData = async (currentFighter: string) => {
-      const result = await axios.get(`https://the-ultimate-api.herokuapp.com/api/fighters/data/moves?fighter=${currentFighter}`);
-      return result.data
+      try {
+        const { status, data } = await axios.get(`https://the-ultimate-api.herokuapp.com/api/fighters/data/moves?fighter=${currentFighter}`);
+        if(status !== 200) throw new Error()
+        console.log(data)
+        return data
+      } catch(e: any) {
+        return e.response.data
+      }
     }
-    nock('https://the-ultimate-api.herokuapp.com')
+    const scope = nock('https://the-ultimate-api.herokuapp.com')
+      .persist()
       .get('/api/fighters/data/moves?fighter=inkling')
       .reply(200, {
         fighter: 'inkling',
         fighterId: 25,
         displayName: 'Inkling',
         rosterId: 75
-      },
-      { 'Access-Control-Allow-Origin': '*' })
-    const result = await fetchData('inkling');
+      })
+      // { 'Access-Control-Allow-Origin': '*' })
+    const result: any = await fetchData('inkling');
     expect(result.fighter).toBe('inkling');
     expect(result).toEqual(
       expect.objectContaining({
@@ -47,12 +54,29 @@ describe('Testing moves data fetch', () => {
         .get('/api/fighters/data/moves?fighter=inkling')
         .reply(400, {
           error: 'fetch failed!'
-        },
-        { 'Access-Control-Allow-Origin': '*' })
+        })
+        // { 'Access-Control-Allow-Origin': '*' })
       const result: any = await fetchData('inkling');
       expect(result.error).toBe('fetch failed!');
     })
-  it.skip('testing', async () => {
+  // it('testing', async () => {
+  //   nock('http://www.google.com')
+  //     .persist()
+  //     .get('/cat-poems')
+  //     .reply(404, {error: 'fetch failed!'})
+  //   async function fetchData() {
+  //     try {
+  //       const { status, data } = await axios.get('http://www.google.com/cat-poems')
+  //       if(status !== 200) throw new Error('Fetch failed!')
+  //       expect(data).toBeTruthy()
+  //     } catch(e: any) {
+  //       return e.response.data
+  //     }
+  //   }
+  //   const result = await fetchData()
+  //   expect(result.error).toBe('fetch failed!')
+  // })
+  it('testing', async () => {
     nock('http://www.google.com')
       .persist()
       .get('/cat-poems')
