1681ab6100c94fde74344cd403813955f8e043c3
DreSean-Sutton <dreseansutton@gmail.com>
2022-09-01 04:31:32 -0700

    Tested out abort controller and got it working with nock

--- client/src/components/moves-data.test.tsx
+++ client/src/components/moves-data.test.tsx
@@ -2,22 +2,25 @@ import nock from 'nock'
 import axios from 'axios';
 axios.defaults.adapter = require('axios/lib/adapters/http')
 
-describe.only('Testing moves data fetch', () => {
-  afterAll(nock.restore);
+// Note to self: console.log increases test response delay
+
+describe('Testing moves data fetch', () => {
+  // afterAll(nock.restore);
   afterEach(nock.cleanAll);
 
+  const controller = new AbortController()
+  async function fetchData(currentFighter: string) {
+    const { status, data } = await axios.get(`https://the-ultimate-api.herokuapp.com/api/fighters/data/moves?fighter=${currentFighter}`, {
+      signal: controller.signal,
+      validateStatus: () => true
+    });
+    if (status !== 200) {
+      return { error: 'fetch failed!' }
+    }
+    return data
+  }
 
   it('Correctly sends data on 200 status', async () => {
-    const fetchData = async (currentFighter: string) => {
-      try {
-        const { status, data } = await axios.get(`https://the-ultimate-api.herokuapp.com/api/fighters/data/moves?fighter=${currentFighter}`);
-        if(status !== 200) throw new Error()
-        console.log(data)
-        return data
-      } catch(e: any) {
-        return e.response.data
-      }
-    }
     const scope = nock('https://the-ultimate-api.herokuapp.com')
       .persist()
       .get('/api/fighters/data/moves?fighter=inkling')
@@ -39,66 +42,15 @@ describe.only('Testing moves data fetch', () => {
       })
     )
   })
-    it('Correctly returns error', async () => {
-      const fetchData = async (currentFighter: string) => {
-        try {
-          const { status, data } = await axios.get(`https://the-ultimate-api.herokuapp.com/api/fighters/data/moves?fighter=${currentFighter}`);
-          if(status !== 200) throw new Error()
-          return data;
-        } catch(e: any) {
-          return e.response.data
-        }
-      }
-      const scope = nock('https://the-ultimate-api.herokuapp.com')
-        .persist()
-        .get('/api/fighters/data/moves?fighter=inkling')
-        .reply(400, {
-          error: 'fetch failed!'
-        })
-        // { 'Access-Control-Allow-Origin': '*' })
-      const result: any = await fetchData('inkling');
-      expect(result.error).toBe('fetch failed!');
-    })
-  it('testing', async () => {
-    nock('http://www.google.com')
+  it('Correctly returns error', async () => {
+    const scope = nock('https://the-ultimate-api.herokuapp.com')
       .persist()
-      .get('/cat-poems')
-      .reply(404, {error: 'fetch failed!'})
-    async function fetchData() {
-      try {
-        const { status, data } = await axios.get('http://www.google.com/cat-poems')
-        if(status !== 200) throw new Error('Fetch failed!')
-        expect(data).toBeTruthy()
-      } catch(e: any) {
-        return e.response.data
-      }
-    }
-    const result = await fetchData()
-    expect(result.error).toBe('fetch failed!')
-  })
-})
-
-describe('Testing out abort controller', () => {
-  afterAll(nock.restore);
-  afterEach(nock.cleanAll);
-
-  const controller = new AbortController()
-  async function fetchDetailsData(currentFighter: string) {
-    // setIsLoading(true)
-    const { status, data } = await axios.get(`https://the-ultimate-api.herokuapp.com/api/fighters/data/moves?fighter=${currentFighter}`, {
-      signal: controller.signal,
-      validateStatus: () => true
-    });
-    if (status !== 200) {
-
-    }
-    // setIsLoading(false)
-    // setMoves(data)
-  }
-  // fetchDetailsData(props.currentFighter)
-  // return () => controller.abort()
-
-  it('sends data on 200 status request', async () => {
-
+      .get('/api/fighters/data/moves?fighter=inklingsssss')
+      .reply(400, {
+        error: 'fetch failed!'
+      })
+      // { 'Access-Control-Allow-Origin': '*' })
+    const result: any = await fetchData('inklingsssss');
+    expect(result.error).toBe('fetch failed!');
   })
 })
