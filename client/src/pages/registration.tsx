10dd507f37e98dd76e880626b100b0f3d549f9a4
DreSean-Sutton <dreseansutton@gmail.com>
2022-09-17 21:11:19 -0700

    Added email and password inputs

--- client/src/pages/registration.tsx
+++ client/src/pages/registration.tsx
@@ -10,27 +10,38 @@ export default function Registration() {
     <Container>
       <Row className='justify-content-center'>
         <Col md={8} lg={6}>
-          <Card className='bg-white p-3 mt-4'>
+          <Card className='bg-white p-3 mb-5'>
             <Card.Title className='text-center'>
               Create an Account
             </Card.Title>
+            {/* Later change Card.Body into its own component */}
             <Card.Body>
               <Form>
-                <Form.Group className="mb-3" controlId="formRegistrationEmail">
+                <Form.Group className='mb-3' controlId='formRegistrationUsername'>
                   <Form.Label>Username</Form.Label>
-                  <Form.Control type="text" placeholder="Username" />
+                  <Form.Control type='text' placeholder='Username' />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formRegistrationPassword">
+                <Form.Group className='mb-3' controlId='formRegistrationEmail'>
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type='email' placeholder='Email' />
+                </Form.Group>
+
+                <Form.Group className='mb-3' controlId='formRegistrationPassword'>
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
-                  <Form.Text className="text-muted">
+                  <Form.Control type='password' placeholder='Password' />
+                  <Form.Text className='text-muted'>
                     Must be 8-20 characters.
                   </Form.Text>
                 </Form.Group>
 
+                <Form.Group className='mb-3'>
+                  <Form.Label>Re-type Password</Form.Label>
+                  <Form.Control type='password' placeholder='Password' />
+                </Form.Group>
+
                 <div className='text-end'>
-                  <Button variant="primary" type="submit">
+                  <Button variant='primary' type='submit'>
                     Create
                   </Button>
                 </div>
