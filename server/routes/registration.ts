a1ba8e01c2430d2b2a9fc4d3e9797bf901312e8b
DreSean-Sutton <dreseansutton@gmail.com>
2022-09-23 18:43:55 -0700

    Added argon2 and used it to now hash passwords sent to database

--- server/routes/registration.ts
+++ server/routes/registration.ts
@@ -2,16 +2,17 @@ var express = require('express');
 const registration = express.Router()
 const dbo = require('../db/conn');
 const { ObjectId } = require('mongodb');
-
+const argon2 = require('argon2')
 // ADD ACCOUNTS
 registration
   .route('/add/account')
-  .post(function (req: any, response: any) {
+  .post(async function (req: any, response: any) {
     let db_connect = dbo.getDb();
+    const hashedPassword = await argon2.hash(req.body.password)
     let myobj = {
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPassword,
     };
     db_connect
       .collection('profiles')
