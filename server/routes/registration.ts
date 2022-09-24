7f89725013eefd13f7da0e9434ba9eee08a11607
DreSean-Sutton <dreseansutton@gmail.com>
2022-09-23 23:28:17 -0700

    It looks dumb, but registration route now checks if username or email exist before inserting

--- server/routes/registration.ts
+++ server/routes/registration.ts
@@ -1,25 +1,51 @@
+import { profile } from "console";
+
 var express = require('express');
 const registration = express.Router()
 const dbo = require('../db/conn');
 const { ObjectId } = require('mongodb');
-const argon2 = require('argon2')
+const argon2 = require('argon2');
+
 // ADD ACCOUNTS
 registration
   .route('/add/account')
   .post(async function (req: any, response: any) {
     let db_connect = dbo.getDb();
     const hashedPassword = await argon2.hash(req.body.password)
-    let myobj = {
+    let profileObj = {
       username: req.body.username,
       email: req.body.email,
       password: hashedPassword,
     };
     db_connect
       .collection('profiles')
-      .insertOne(myobj, function (err: any, res: any) {
-        if (err) throw err;
-        response.json(res);
-      });
+      .findOne({ username: profileObj.username }, function (err: any, res: any) {
+        console.log('findOne1 value: ', res);
+        if(err) throw err;
+        if(res) {
+          response.json(res.username);
+        } else {
+          console.log('This worked!');
+          db_connect
+            .collection('profiles')
+            .findOne({ email: profileObj.email }, (err: any, res: any) => {
+              console.log('findOne2 value: ', res);
+              if(err) throw err;
+              if(res) {
+                response.json(res.username);
+              } else {
+                console.log('This worked, too!');
+                db_connect
+                  .collection('profiles')
+                  .insertOne(profileObj, function (err: any, res: any) {
+                    if (err) throw err;
+                    console.log(res);
+                    response.json(res);
+                  });
+              }
+            })
+        }
+      })
   });
 
   module.exports = registration
