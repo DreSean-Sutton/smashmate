57dedfee4ee0d13bb39f54d1dfce7b7ecd776886
DreSean-Sutton <dreseansutton@gmail.com>
2022-09-23 18:07:18 -0700

    Tests are correctly working with nock and handling errors

--- client/src/components/registration/registration.test.tsx
+++ client/src/components/registration/registration.test.tsx
@@ -5,35 +5,53 @@ expect.extend(matchers);
 axios.defaults.adapter = require('axios/lib/adapters/http')
 
 describe.only('Registration creation route', () => {
-  // afterEach(nock.cleanAll);
+  afterEach(nock.cleanAll);
 
-  const controller = new AbortController()
-  async function sendAccountDetails(url: string, profile: any) {
-    const result = await axios.post(url, profile, {
-      signal: controller.signal,
-      validateStatus: () => true
-    });
-    // console.error(result);
-    if (result.status !== 200) {
-      return { error: `Account creation failed!` }
-    }
-    return result.data;
-  }
+  const url = 'http://localhost:5000/registration/add/account';
   const myProfile = {
     username: 'Dre Sean',
     email: 'dreseansutton@gmail.com',
     password: 'test password'
   }
+  async function sendAccountDetails() {
+    try {
+      const controller = new AbortController()
+      const result = await axios.post(url, myProfile, {
+        signal: controller.signal,
+        validateStatus: () => true
+      });
+      if (result.status !== 200) throw new Error('Account creation failed!')
+      return result.data;
+    } catch(e: any) {
+      return { error: e.message }
+    }
+  }
+
+
   it('responds with correct data on 200 status code', async () => {
-    const url = 'http://localhost:5000/registration/add/account';
-    const result = await sendAccountDetails(url, myProfile);
-    console.log(result);
+    nock('http://localhost:5000')
+      .persist()
+      .post('/registration/add/account')
+      .reply(200, {
+        acknowledged: true,
+        insertedId: 'sdfisal2904'
+      });
+
+    const result = await sendAccountDetails();
     expect(result.acknowledged).toEqual(true);
     expect(result.insertedId).toBeTruthy();
+    expect(result.error).toBeUndefined();
   })
-  it('responds to the user if url isn\'t found', async () => {
-    const url = 'http://localhost:5000/registration/add/accounts';
-    const result = await sendAccountDetails(url, myProfile);
-    expect(result.error).toBeTruthy();
+
+  it('responds with error message on 404 status code', async () => {
+    nock('http://localhost:5000')
+      .persist()
+      .post('/registration/add/account')
+      .reply(400, {
+        error: 'Account creation failed!'
+      });
+
+    const result = await sendAccountDetails();
+    expect(result.error).not.toBeUndefined();
   })
 })
