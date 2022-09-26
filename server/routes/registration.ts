7cf6ff54d56d486cb215e10449484760c3bb4adb
DreSean-Sutton <dreseansutton@gmail.com>
2022-09-26 03:33:59 -0700

    Refactored Add accounts route

--- server/routes/registration.ts
+++ server/routes/registration.ts
@@ -11,7 +11,7 @@ const argon2 = require('argon2');
 // ADD ACCOUNTS
 registrationRoute
 .route('/account/add')
-.post(async function (req: any, response: any) {
+.post(async function (req: any, response: any, next: any) {
   let db_connect = dbo.getDb();
   const hashedPassword = await argon2.hash(req.body.password)
     let profileObj = {
@@ -19,39 +19,38 @@ registrationRoute
       email: req.body.email,
       password: hashedPassword,
     };
-    db_connect
-      .collection('profiles')
-      .findOne({ username: profileObj.username }, function (err: any, res: any) {
-        if(err) throw err;
-        if(res) {
-          response.json({ username: res.username });
-        } else {
-          console.log('This worked!');
+    try {
+      db_connect
+        .collection('profiles')
+        .findOne({ username: profileObj.username }, function (err: any, res: any) {
+          if(err) throw err;
+          if(res) {
+            return response.status(400).json({ username: res.username });
+          }
           db_connect
             .collection('profiles')
             .findOne({ email: profileObj.email }, (err: any, res: any) => {
               if(err) throw err;
               if(res) {
-                response.json({ email: res.email });
-              } else {
-                db_connect
-                  .collection('profiles')
-                  .insertOne(profileObj, function (err: any, res: any) {
-                    if (err) throw err;
-                    console.log(res);
-                    response.json(res);
-                  });
+                return response.status(400).json({ email: res.email });
               }
+              db_connect
+                .collection('profiles')
+                .insertOne(profileObj, function (err: any, res: any) {
+                  if (err) throw err;
+                  response.status(201).json(res);
+                });
             })
-        }
-      })
+        })
+      } catch (e) {
+        next(e);
+      }
   });
 
 registrationRoute
   .route('/account/sign-in')
   .get(function (req: any, response: any, next: any) {
     let db_connect = dbo.getDb();
-    console.log('req.query value: ', req.query.email);
     const { email, password } = req.query;
     db_connect
     .collection('profiles')
@@ -59,11 +58,11 @@ registrationRoute
       try {
         if (err) throw err;
         if (!res) {
-          throw new ClientError(404, 'Email not found');
+          throw new ClientError(400, 'Email not found');
         }
         response.status(200).json(res);
       } catch(e) {
-        return next(e);
+        next(e);
       }
     })
   })
