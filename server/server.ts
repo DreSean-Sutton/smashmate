0b7bdb6f1491ca432ac9aa675d130baeb5ece704
DreSean-Sutton <dreseansutton@gmail.com>
2022-10-21 00:46:52 -0700

    Testing out removing cors support from backend

--- server/server.ts
+++ server/server.ts
@@ -7,12 +7,12 @@ var dbo = require('./db/conn');
 const port = process.env.PORT || 5001;
 const app = express();
 app.use(express.json());
-app.use(function (req: any, res: any, next: any) {
-  // Required to bypass CORS
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function (req: any, res: any, next: any) {
+//   // Required to bypass CORS
+//   res.header("Access-Control-Allow-Origin", '*');
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 const registrationRoute = require('./routes/registration');
 const favoritingRoute = require('./routes/favoriting');
