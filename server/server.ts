f223ebb6d04b2c82b29b4a2cbf1ce959593e1941
DreSean-Sutton <dreseansutton@gmail.com>
2022-10-20 17:40:52 -0700

    Successfully completed deployment, but some errors

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,7 @@
 import ClientError from "./client-error";
 import errorMiddleware from "./error-middleware";
 const path = require('path');
-require('dotenv').config({ path: '../../.env' });
+require('dotenv').config({ path: '../.env' });
 var express = require('express');
 var dbo = require('./db/conn');
 const port = process.env.PORT || 5001;
@@ -19,10 +19,10 @@ const favoritingRoute = require('./routes/favoriting');
 app.use('/registration', registrationRoute);
 app.use('/favoriting', favoritingRoute);
 
-// if(process.env.NODE_ENV === 'production') {
-//   // serve files from the client's build dir
-//   app.use(express.static(__dirname, '../../client/build'));
-// }
+if(process.env.NODE_ENV === 'production') {
+  // serve files from the client's build dir
+  app.use(express.static(__dirname, '../../client/build'));
+}
 app.use('/api', (req: any, res: any) => {
   res.status(404).json({ error: `cannot ${req.method} ${req.url}` })
 });
