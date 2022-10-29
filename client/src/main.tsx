156bd75d39176029ef268a6839c4f65754dfaee3
DreSean-Sutton <dreseansutton@gmail.com>
2022-10-29 07:59:43 -0700

    Frontend is working correctly

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,22 +4,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // React.StrictMode makes useEffect render twice per render in dev mode, but not production mode
+  // React.StrictMode makes useEffect render twice per react render in dev mode, but not production mode
   <Router>
     <Provider store={store}>
       <App />
     </Provider>
   </Router>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
