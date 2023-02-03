a2edb8d49e35bcfb2b5e472044f1f9b6040db84c
DreSean-Sutton <dreseansutton@gmail.com>
2023-02-03 02:19:40 -0800

    Finished the setup for favoriting.test.ts file

--- server/__tests__/routes/favoriting.test.ts
+++ server/__tests__/routes/favoriting.test.ts
@@ -1,5 +1,43 @@
-describe('nothing here', () => {
-  it("does nothing", () => {
+var { request, nock, port } = require('../test.config');
 
+describe.only("Favoriting route: POST /api/favoriting/characters/upsert", () => {
+
+  afterAll(nock.cleanAll);
+
+  const baseURL = `http://localhost:${port}`;
+  const postURL = '/api/favoriting/characters/upsert';
+  async function fetchData() {
+    const res = await request(baseURL)
+      .post(postURL)
+      .send({
+
+      })
+    return res;
+  }
+  describe("", () => {
+    it("does nothing", async () => {
+
+    })
+  })
+})
+
+describe("Favoriting route: POST /api/favoriting/characters/get", () => {
+
+  afterAll(nock.cleanAll);
+
+  const baseURL = `http://localhost:${port}`;
+  const postURL = '/api/favoriting/characters/upsert';
+  async function fetchData() {
+    const res = await request(baseURL)
+      .post(postURL)
+      .send({
+
+      })
+    return res;
+  }
+  describe("", () => {
+    it("does nothing", async () => {
+
+    })
   })
 })
