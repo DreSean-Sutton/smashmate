a4ebb7e7aeb4deeeb7fe9927b0120bf9cdabf205
DreSean-Sutton <dreseansutton@gmail.com>
2023-03-20 22:31:39 -0700

    Added expectations to SiteNavbar tests

--- client/src/__tests__/components/navbar/SiteNavbar.test.tsx
+++ client/src/__tests__/components/navbar/SiteNavbar.test.tsx
@@ -12,10 +12,10 @@ import { renderWithProviders } from '../../../util/test-utils';
 describe('Testing SiteNavbar UI/UX', () => {
   it('renders SiteNavbar', async () => {
     renderWithProviders(<SiteNavbar />)
-    screen.getByText(/guest/i)
-    screen.getByText(/home/i)
-    screen.getByText(/favorites/i)
-    screen.getByText(/login/i)
-    screen.getByTestId(/profile-icon/)
+    expect(screen.getByText(/guest/i)).toBeInTheDocument();
+    expect(screen.getByText(/home/i)).toBeInTheDocument();
+    expect(screen.getByText(/favorites/i)).toBeInTheDocument();
+    expect(screen.getByText(/login/i)).toBeInTheDocument();
+    expect(screen.getByTestId(/profile-icon/)).toBeInTheDocument();
   })
 })
