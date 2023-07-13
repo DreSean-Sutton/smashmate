8c43c951709b2b8cd9fe30f1d49b45ac78a205d0
DreSean-Sutton <dreseansutton@gmail.com>
2023-07-13 03:18:00 -0700

    Added a test that closes the DataModal component when the outside of it is clicked

--- client/src/__tests__/UI-tests/pages/FighterDetails.ui.test.tsx
+++ client/src/__tests__/UI-tests/pages/FighterDetails.ui.test.tsx
@@ -13,18 +13,19 @@ import FighterDetails from '../../../pages/FighterDetails';
 
 describe("Testing fighterDetails", () => {
 
-  describe("Walking through bayonetta's data", () => {
+  describe("Walking through Bayonetta's data", () => {
 
-    it("renders bayonetta's data", async () => {
+    it("renders Bayonetta's Moves data", async () => {
       renderWithProviders(<FighterDetails />);
-      const moves = await screen.findByText(/^bayonetta's moves$/i);
+      const moves = await (waitFor(() => screen.findByText(/^bayonetta's moves$/i), { timeout: 3000 }));
+      // const moves = await screen.findByText(/^bayonetta's moves$/i);
       const movesTable = await screen.findByTestId(/^moves-table$/);
       expect(moves).toBeInTheDocument();
       expect(movesTable).toBeInTheDocument();
       expect(screen.getByRole('table')).toBeInTheDocument();
     });
 
-    it("opens the DataModel component when the data navbar is clicked", async () => {
+    it("opens the DataModal component when the data navbar is clicked", async () => {
       renderWithProviders(<FighterDetails />);
       const dataNavbar = await screen.findByTestId(/data-navbar/);
       expect(dataNavbar).toBeInTheDocument();
@@ -35,7 +36,25 @@ describe("Testing fighterDetails", () => {
       expect(screen.getByRole('button', { name: /throws/i })).toBeInTheDocument();
       expect(screen.getByRole('button', { name: /movements/i })).toBeInTheDocument();
       expect(screen.getByRole('button', { name: /stats/i })).toBeInTheDocument();
-    })
+    });
+
+    it("closes the DataModal component when the outside of the modal is clicked", async () => {
+      renderWithProviders(<FighterDetails />);
+      userEvent.click(await screen.findByTestId(/data-navbar/));
+      const dataModal = await screen.findByTestId(/data-modal/);
+      const modalBackdrop = document.querySelector('.modal-backdrop');
+      await act(() => userEvent.click(modalBackdrop));
+      expect(dataModal).not.toBeInTheDocument();
+    });
+
+    it("renders Bayonetta's Throw data when DataModal's throw button is clicked", async () => {
+      renderWithProviders(<FighterDetails />);
+      userEvent.click(await screen.findByTestId(/data-navbar/));
+      const dataModal = await screen.findByTestId(/data-modal/);
+      const throwButton = await screen.findByRole('button', { name: /throws/i });
+      await act(() => userEvent.click(throwButton));
+      expect(dataModal).not.toBeInTheDocument();
+    });
 
   })
 });
